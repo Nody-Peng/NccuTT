c2873fee7cbe30d5fb10ba951161dd897fc4d32a
Nody Peng <ablecck12@gmail.com>
2025-01-20 19:41:53 +0800

    doc: 移除client, server資料夾；修改frontend資料夾名稱

--- client/src/context/kits.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const formatDate = (date: Date): string => {
-  return new Intl.DateTimeFormat('ja-JP', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  }).format(date)
-}
